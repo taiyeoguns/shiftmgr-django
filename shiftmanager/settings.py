0aeb7f938ea2c25f1370888949991e83e40c37ec
Taiye <taiyeoguns@yahoo.com>
2018-08-16 20:46:34 +0100

    Static and template changes

--- shiftmanager/settings.py
+++ shiftmanager/settings.py
@@ -129,4 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
