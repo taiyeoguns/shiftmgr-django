68f75d293d3b10cbb5413ff6ec168172f877af72
Taiye <taiyeoguns@yahoo.com>
2018-08-23 23:35:49 +0100

    Added readme and some migrations for shift app

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    # return HttpResponse("<h2>home page</h2>")
     return render(request, 'site/index.html', {})
