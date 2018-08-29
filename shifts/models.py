c4ade0488dcd96d66e79b8f00e91d4601ff90ca8
Taiye <taiyeoguns@yahoo.com>
2018-08-29 10:21:34 +0100

    Modified models

--- shifts/models.py
+++ shifts/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+
+class Manager(models.Model):
+    """ Manager """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Member(models.Model):
+    """ Member """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class Shift(models.Model):
     """Shift object"""
 
+    manager = models.ForeignKey('Manager', on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now, help_text='Enter Shift Date')
 
     def __str__(self):
-        return self.date
+        return str(self.date)
 
 
 class Task(models.Model):
