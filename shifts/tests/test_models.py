a4830f9a64915367616e994f8f9ebca4b2c14d4f
Taiye <taiyeoguns@yahoo.com>
2018-10-25 10:43:06 +0100

    Added few model tests

--- shifts/tests/test_models.py
+++ shifts/tests/test_models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
+from ..models import Profile
+import pytest
+
+
+@pytest.mark.django_db
 class TestModels:
-    def test_dummy(self):
-        assert True
+    def test_profile_is_created_when_user_is_created(self):
+
+    	user = User.objects.create_user(username='testuser', email='testuser@email.com', password='password')
+
+    	assert isinstance(user.profile, Profile)
+    	assert user.profile is not None
+
+    def test_profile_is_deleted_when_user_is_deleted(self):
+    	user = User.objects.create_user(username='testuser', email='testuser@email.com', password='password')
+    	user.delete()
+
+    	with pytest.raises(Profile.DoesNotExist):
+	    	#user.refresh_from_db()
+	    	Profile.objects.get(user=user)
+
+
