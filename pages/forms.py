5eced31d8d104a31af9eebb79b5d694884ab6cf5
Taiye <taiyeoguns@yahoo.com>
2018-11-06 17:05:25 +0000

    Setting up registration

---
+++ pages/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=25, required=True)
+    first_name = forms.CharField(max_length=25, required=True)
+    last_name = forms.CharField(max_length=25, required=True)
+    phone = forms.CharField(max_length=25)
+    email = forms.EmailField(max_length=75, required=True)
+    password1 = forms.CharField(max_length=25, required=True)
+    password2 = forms.CharField(max_length=25, required=True)
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Username already exists")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("Email already exists")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
--- shiftmanager/urls.py
+++ shiftmanager/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, auth
+from django.contrib import admin
 from django.urls import path, include
 from pages import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('auth/register', views.register, name='register'),
     path('auth/', include('django.contrib.auth.urls')),
     path('shifts/', include('shifts.urls'))
 ]
