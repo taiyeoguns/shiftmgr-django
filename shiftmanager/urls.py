a423c288c54605b0892c8a694e6e64e254886b61
Taiye <taiyeoguns@yahoo.com>
2018-12-06 20:39:17 +0000

    Changed pages app name to base

--- shiftmanager/urls.py
+++ shiftmanager/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pages import views
+from base import views
 
 urlpatterns = [
     path('', views.home, name='home'),
